99e5c4f07370732c84810ad444861c79dc989e61
Dina <dina.bitca@isa.utm.md>
2023-03-22 09:04:32 +0200

    Added third week tasks

--- src/main/scala/Laboratory2/Week1/PrinterActor.scala
+++ src/main/scala/Laboratory2/Week1/PrinterActor.scala
@@ -3,26 +3,45 @@ package Week1
 import akka.actor.{Actor, ActorLogging, Props}
 import io.circe.Json
 import io.circe.parser._
+
+import java.util
 class PrinterActor extends Actor with ActorLogging {
+  private val bad_words: Array[String] = Array(
+    "arse", "arsehead", "arsehole", "ass", "asshole", "bastard", "bitch", "bloody", "bollocks", "brotherfucker",
+    "bugger", "bullshit", "child-fucker", "Christ on a bike", "Christ on a cracker", "cock", "cocksucker", "crap",
+    "cunt", "damn", "damn it", "dick", "dickhead", "dyke", "fatherfucker", "frigger", "fuck", "goddamn", "godsdamn",
+    "hell", "holy shit", "horseshit", "in shit", "Jesus Christ", "Jesus fuck", "Jesus H. Christ", "Jesus Harold Christ",
+    "Jesus wept", "Jesus, Mary and Joseph", "kike", "motherfucker", "nigga", "nigra", "piss", "prick", "pussy", "shit",
+    "shit ass", "shite", "sisterfucker", "slut", "son of a bitch", "son of a whore", "spastic", "sweet Jesus", "turd",
+    "twat", "wanker", "fucking", "shitting")
   override def receive: Receive = {
+
     case tweetData: String =>
-     // println(s"Received tweet data: $tweetData")
+      //     println(s"Received tweet data: $tweetData")
       val pattern = """"text":"(.+?)"""".r
       pattern.findFirstMatchIn(tweetData) match {
         case Some(matched) =>
-          val tweetText = matched.group(1)
-          println(s"Tweet text: $tweetText")
-        case None =>
-          println("No tweet text found.")
+          var tweetText = matched.group(1)
+          for (bad_word <- bad_words) {
+            val bad_word_chars = bad_word.split("")
+            val replacement = bad_word_chars.map(_ => "*").mkString("")
+            tweetText = tweetText.split("\\s+")
+              .map(word => if (word.toLowerCase == bad_word.toLowerCase) replacement else word)
+              .mkString(" ")
+          }
+          println(s"Tweet from:  " + self.path.name + s" $tweetText")
+          Thread.sleep(1000)
       }
-      //generating random sleep time between 5ms and 50 ms
-      val sleepTime = scala.util.Random.nextInt(46) + 5
-      Thread.sleep(sleepTime)
+    case None =>
+      throw new Exception("No tweet text found message")
   }
+  //generating random sleep time between 5ms and 50 ms
+  val sleepTime = scala.util.Random.nextInt(46) + 5
+  Thread.sleep(sleepTime)
 }
 
-  object PrinterActor {
-    def props(): Props = Props(new PrinterActor())
-    case object Send
-  }
+object PrinterActor {
+  def props(): Props = Props(new PrinterActor())
+  case object Send
+}
 
--- src/main/scala/Laboratory2/Week2/Pool_Supervisor.scala
+++ src/main/scala/Laboratory2/Week2/Pool_Supervisor.scala
@@ -1,38 +1,75 @@
 package Laboratory2.Week2
 
-import Week1.{PrinterActor, StreamReader}
-import Week2.Pool_Supervisor.Print
+import Week1.PrinterActor
 import akka.actor.SupervisorStrategy.Restart
 import akka.actor.{Actor, ActorRef, OneForOneStrategy, Props}
 import akka.protobufv3.internal.TextFormat.printer
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 import java.time.InstantSource.system
 import scala.concurrent.duration.DurationInt
 
 class Pool_Supervisor extends Actor{
   import Pool_Supervisor._
+  var currentNrofTweets = 0
+  var nrofActors = 3
 
   val strategy = OneForOneStrategy(maxNrOfRetries = 3, withinTimeRange = 1.minute) {
     case _: Exception => Restart
   }
-  val printers: Seq[ActorRef] = (1 to 3).map { i =>
+  var printers: Seq[ActorRef] = (1 to nrofActors).map { i =>
     val printer = context.actorOf(Props[PrinterActor], s"printer$i")
     context.watch(printer)
     printer
   }
 
   var nextPrinterIndex = 0;
+
+  context.system.scheduler.scheduleWithFixedDelay(
+    0.seconds,
+    10.seconds,
+    self,
+    AdjustActors
+  )(context.dispatcher)
   override def receive: Receive = {
- //create printer pool
+    //create printer pool
     case (message : String) =>
-  {
-    val printer = printers (nextPrinterIndex)
-    printer ! message
-  }
-      nextPrinterIndex = (nextPrinterIndex + 1) % 3
+    {
+      currentNrofTweets +=1
+      val printer = printers (nextPrinterIndex)
+      printer ! message
+      //    context.system.scheduler.scheduleOnce(2.seconds, self, (message: String))
+    }
+
+    case AdjustActors =>{
+      if(currentNrofTweets > nrofActors * 4){
+        nrofActors += 1
+        //        println("am intrat in case ul creste nr of Actors:" + nrofActors)
+        val newPrinter = context.actorOf(Props[PrinterActor], s"printer$nrofActors")
+        //        println("am creat un nou actor: " + s"printer$nrofActors")
+        context.watch(newPrinter)
+        printers = printers :+ newPrinter
+        println(s"Added new printer, total nr of printers: $nrofActors")
+      }
+      else if(currentNrofTweets < nrofActors * 2){
+        if(nrofActors > 1){
+          val printerToKill = printers.last
+          //          printerToKill ! akka.actor.PoisonPill
+          printers = printers.filterNot(_ == printerToKill)
+          nrofActors -=1
+          //          println("am intrat in case-ul cu micsorarea nrlui de actori: " + nrofActors)
+          println(s"Removed printer, total number of printers: $nrofActors")
+        }
+      }
+      currentNrofTweets = 0
+    }
+
+      nextPrinterIndex = (nextPrinterIndex + 1) % nrofActors
   }
 }
 object Pool_Supervisor {
   def props(): Props = Props(new Pool_Supervisor())
   case object Print
+  case object AdjustActors
 }
